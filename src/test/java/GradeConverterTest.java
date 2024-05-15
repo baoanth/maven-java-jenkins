dbeac2f9db54f5b0d4b076f3cbd46d6ed107c1ee
baoanth <baoanth@gmail.com>
2024-05-15 14:31:53 +0700

    Init project

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+public class GradeConverterTest {
+    private GradeConverter gradeConverter;
+
+    @Before
+    public void setUp() throws Exception {
+        gradeConverter = new GradeConverter();
+    }
+
+    @Test
+    public void testConvert1() throws Exception {
+        String expected = "A";
+        String actual = gradeConverter.convert(100);
+        assertEquals(expected,actual);
+
+
+    }
+}
